fbb2fee2c1d903a4508366eaa0f2ecf81312e807
KosieVich <junkjuk5645@gmail.com>
2023-06-05 23:45:00 +0300

    add tests

---
+++ sw_course_test/ReaderFromFileTest.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using sw_course;
+
+namespace sw_course_test;
+
+public class ReaderFromFileTest
+{
+    [Test]
+    public void Test1()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "testLogEmpty.tlog");
+        using var reader = new ReaderFromFile(file);
+        Assert.AreEqual( reader.GetMessagesInCsvFormat().Count(), 1);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(), "testLog.tlog");
+        using var reader = new ReaderFromFile(file);
+        Assert.AreEqual(reader.GetMessagesInCsvFormat().Count(), 698);
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        Assert.Throws<FileNotFoundException>((() => _ = new ReaderFromFile("blablabla")));
+    }
+}
