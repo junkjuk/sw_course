1244fe5654501634d03366d2fade8512052eaa18
KosieVich <junkjuk5645@gmail.com>
2023-06-05 23:44:54 +0300

    Add writing to output file

--- sw_course/Program.cs
+++ sw_course/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using sw_course;
+
+var outFileName = "test.csv";
+
+using var reader = new ReaderFromFile(Path.Combine(Directory.GetCurrentDirectory(), "testLog.tlog"));
+
+using var fs = new StreamWriter(outFileName);
+foreach (var messageLine in reader.GetMessagesInCsvFormat())
+{
+    fs.WriteLine(messageLine);
+}
